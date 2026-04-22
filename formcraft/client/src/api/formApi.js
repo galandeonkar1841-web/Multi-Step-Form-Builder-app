bf871d98b50bab4e0befc3caebcd0443b1d152c9
galandeonkar1841-web <galandeonkar1841@gmail.com>
2026-04-22 10:11:41 +0530

    update backend api url

--- formcraft/client/src/api/formApi.js
+++ formcraft/client/src/api/formApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://formcraft-backend-xxxx.onrender.com/api' })
+const api = axios.create({ baseURL: 'https://formcraft-backend-z0zv.onrender.com/api' })
 
 export const formApi = {
   getAll: () => api.get('/forms'),
